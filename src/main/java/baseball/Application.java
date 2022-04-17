aff058108ad54d59a03cdacb49a84f5963a28d72
berrynana <parkjiyoon7@gmail.com>
2022-04-17 20:44:17 +0900

    feat(common): main class of this game
    Modify Application class
    - call the create() of MakeRandomNumber class
    - repeat until user match the number and enter 2
    - call the tryAnswer() of ProcessGame class

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import baseball.domain.MakeRandomNumber;
+import baseball.domain.Playagain;
+import baseball.domain.ProcessGame;
+import java.util.List;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean again = true;
+
+        while (again) {
+            List<Integer> computer = MakeRandomNumber.create();
+            ProcessGame.tryAnswer(computer);
+            again = Playagain.isContinue();
+        }
     }
 }
